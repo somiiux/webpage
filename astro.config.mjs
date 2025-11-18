192036695b87528ff814097472825b54e677ebec
somiiux <akasomiiux@gmail.com>
2025-11-18 03:57:08 +0100

    Update: +

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+
 export default defineConfig({
+  site: 'https://somiiux.com', // 🔽 AÑADE TU DOMINIO 🔽
+  base: '/',
+  build: {
+    assets: 'assets'
+  },
   vite: {
     logLevel: 'error', // mostra só erros reais, oculta os warnings
   },
